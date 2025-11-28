4735c5c29b437b69238b42bf782ad39222b0b2d7
LONG NGUYEN THANH <longnguyenthanh@LONGs-MacBook-Pro.local>
2025-11-28 23:37:27 +0700

    update địa điểm

--- src/components/EventPlace.tsx
+++ src/components/EventPlace.tsx
@@ -18,24 +18,26 @@ export const EventPlace = () => {
           </div>
 
           <div className="col-md-6">
-            <div className="text-center flex flex-col h-full">
+            <div className="text-center flex flex-col h-full justify-center items-center">
               <h2 className="text-[#a10129] dancing-script-semibold text-[2.5rem] md:text-[3.75rem]">
                 Địa Điểm Tổ Chức
               </h2>
-              <h3 className="text-[1.25rem] mb-5   leading-none">
+              <h3 className="text-[1.25rem] mb-5 leading-none">
                 Trung Tâm Tiệc Cưới & Sự Kiện Promes Center <br />
                 <span className="text-[1rem] dancing-thin text-gray-600 ">
                   122 - 124, Đ. Xuân Thủy, Dịch Vọng Hậu, Cầu Giấy, Hà Nội 10000
                 </span>
               </h3>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.887128703285!2d105.7872311!3d21.0372018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab277ebd8c1b%3A0xa3b26494470bd077!2zVHJ1bmcgVMOibSBUaeG7h2MgQ8aw4bubaSAmIFPhu7EgS2nhu4duIFByb21lcyBDZW50ZXI!5e0!3m2!1sen!2s!4v1764346457498!5m2!1sen!2s"
-                width="550"
-                className="grow"
-                // style={{ border: 0 }}
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-              ></iframe>
+              <div className="w-full max-w-[550px] flex-grow px-4 pt-6">
+                <div className="w-full h-full aspect-video">
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.887128703285!2d105.7872311!3d21.0372018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab277ebd8c1b%3A0xa3b26494470bd077!2zVHJ1bmcgVMOibSBUaeG7h2MgQ8aw4bubaSAmIFPhu7EgS2nhu4duIFByb21lcyBDZW50ZXI!5e0!3m2!1sen!2s!4v1764346457498!5m2!1sen!2s"
+                    className="w-full h-full border-0 rounded-lg shadow-lg"
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                  ></iframe>
+                </div>
+              </div>
             </div>
           </div>
         </div>
